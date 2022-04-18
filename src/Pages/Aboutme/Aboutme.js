574bfd1298d01d7df9a1cd8b191052af889156bb
Armin Sultana <arminsultana556@gmail.com>
2022-04-18 20:29:04 +0600

    About me description done

--- src/Pages/Aboutme/Aboutme.js
+++ src/Pages/Aboutme/Aboutme.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Aboutme.css'
 
 const Aboutme = () => {
     return (
-        <div>
-            <h1>This is about</h1>
+        <div className='About-me text-left'>
+            <h1 className='text-4xl '>Hi! I'm<span> Armin Sultana</span></h1>
+            <p className='text-2xl'>My Future Goal:</p>
+            <p className='font-semibold'>"My future goal is to establish myself as a full stack web developer".</p>
+            <p>How can i work hard: <br />
+                    There will be an allocation of five to six hours a day for learning web development <br />
+                    And every day I will create new projects by searching from Google YouTubebr <br />
+                    There is no substitute for practice,Just <br />
+                   <p className='font-semibold'>Practise! Practise! Practise!</p>
+               
+            </p>
             
         </div>
     );
